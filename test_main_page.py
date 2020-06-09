6910c17bef86033ea01d50f1aad7ca694b29aac3
Evgen277 <instant277@gmail.com>
2020-06-09 23:57:10 +1000

    test_main_page.py added test_guest_cant_see_product_in_basket_opened_from_main_page method

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,24 @@
-from .pages.main_page import MainPage
+from .pages.base_page import BasePage
+import time
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
      link = "http://selenium1py.pythonanywhere.com/"
-     page = MainPage(browser, link)
+     page = BasePage(browser, link)
      page.open()
-     page.should_be_login_link()
+     page.go_to_basket()
+     time.sleep(2)
+
+     
+
+
+
+
+#def test_guest_should_see_login_link(browser):
+     #link = "http://selenium1py.pythonanywhere.com/"
+     #page = MainPage(browser, link)
+     #page.open()
+     #page.should_be_login_link()
 
 #def test_guest_can_go_to_login_page(browser):
     #link = "http://selenium1py.pythonanywhere.com/"
