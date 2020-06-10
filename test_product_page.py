bd9b9f6f926b93db1f21e37618e41279393f60cf
Evgen277 <instant277@gmail.com>
2020-06-10 16:18:40 +1000

    test_product_page.py class TestUserAddToBasketFromProductPage added;

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import time
 import pytest
 
@@ -38,3 +39,40 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
      page.open()
      page.go_to_basket()
      page.should_be_empty_basket()
+
+def test_guest_cant_see_success_message(browser):
+     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+     page = ProductPage(browser, link)
+     page.open()
+     page.should_add_product_to_basket()
+     page.solve_quiz_and_get_code()
+     page.should_not_be_success_message()
+     
+@pytest.mark.login_user
+class TestUserAddToBasketFromProductPage():
+     @pytest.fixture(scope = "function", autouse = True)
+     def setup(self, browser):
+          link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+          page = LoginPage(browser, link)
+          page.open()
+          email = str(time.time()) + "@user.com"
+          password = "123Ea456789"
+          page.register_new_user(email, password)
+          page.should_be_authorized_user()
+          
+     def test_user_cant_see_success_message(self, browser):
+          link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+          page = ProductPage(browser, link)
+          page.open()
+          page.should_add_product_to_basket()
+          page.solve_quiz_and_get_code()
+          page.should_not_be_success_message()
+
+     def test_user_can_add_product_to_basket(self, browser):  
+         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+         page = ProductPage(browser, link)
+         page.open()
+         page.should_add_product_to_basket()
+         page.solve_quiz_and_get_code()
+          
+     
