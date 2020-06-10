21133185107db944f13f5e2a17f1d480f3a37037
Evgen277 <instant277@gmail.com>
2020-06-10 20:39:54 +1000

    basket_page.py Locators added; should_be_empty_basket method added

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import BasePageLocators
+from .locators import ProductPageLocators
 import time
 
 class BasketPage(BasePage):
@@ -10,9 +11,3 @@ class BasketPage(BasePage):
         assert not self.is_element_present(*BasePageLocators.BASKET_ITEMS), "Items list should not be on the empty basket screen"
         assert self.is_element_present(*BasePageLocators.BASKET_MESSAGE), "No empty basket message"
     
-    def def test_guest_cant_see_success_message_after_adding_product_to_basket():
-        add_product = self.browser.find_element(*ProductPageLocators.ADD_PRODUCT)
-        add_product.click()
-        added_message = self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_TO_BASKET)
-        p = added_message.text
-        
