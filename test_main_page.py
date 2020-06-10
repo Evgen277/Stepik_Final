eb3998499f476d6fc1fb501e94fc6206f6099617
Evgen277 <instant277@gmail.com>
2020-06-10 13:37:55 +1000

    test_main_page.py TestLoginFromMainPage class added; @pytest.mark.login_guest added

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,14 @@
 from .pages.base_page import BasePage
+from .pages.basket_page import BasketPage
 import time
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
      link = "http://selenium1py.pythonanywhere.com/"
-     page = BasePage(browser, link)
+     page = BasketPage(browser, link)
      page.open()
      page.go_to_basket()
-     time.sleep(2)
+     page.should_be_empty_basket()
 
      
 
