2c7c6e2bbf238ca6022b4d82373e3f1b4c4bcc28
Evgen277 <instant277@gmail.com>
2020-06-10 12:42:19 +1000

    test_product_page.py page.should_be_empty_basket() added

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import time
 import pytest
 
@@ -33,7 +34,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
      link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-     page = ProductPage(browser, link)
+     page = BasketPage(browser, link)
      page.open()
      page.go_to_basket()
-     time.sleep(2)
+     page.should_be_empty_basket()
